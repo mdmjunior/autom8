2896de2363cb0140625beeaa4443de32908a84bb
Marcio Moreira Junior <mdmjunior@gmail.com>
2026-07-10 13:28:27 -0300

    Improve CLI visual baseline and command flow

--- suite/core/summary.sh
+++ suite/core/summary.sh
@@ -5,6 +5,13 @@ AUTOM8_SUMMARY_FAIL=0
 AUTOM8_SUMMARY_WARN=0
 AUTOM8_SUMMARY_ITEMS=()
 
+autom8_reset_summary() {
+  AUTOM8_SUMMARY_OK=0
+  AUTOM8_SUMMARY_FAIL=0
+  AUTOM8_SUMMARY_WARN=0
+  AUTOM8_SUMMARY_ITEMS=()
+}
+
 autom8_summary_ok() {
   AUTOM8_SUMMARY_OK=$((AUTOM8_SUMMARY_OK + 1))
   AUTOM8_SUMMARY_ITEMS+=("OK: $1")
@@ -23,12 +30,20 @@ autom8_summary_warn() {
 autom8_print_summary() {
   autom8_title "Resumo da execução"
 
-  printf 'Concluídas: %s\n' "$AUTOM8_SUMMARY_OK"
-  printf 'Avisos: %s\n' "$AUTOM8_SUMMARY_WARN"
-  printf 'Falhas: %s\n' "$AUTOM8_SUMMARY_FAIL"
-  printf 'Logs: %s\n\n' "$AUTOM8_LOG_DIR"
+  autom8_key_value "Concluídas" "$AUTOM8_SUMMARY_OK"
+  autom8_key_value "Avisos" "$AUTOM8_SUMMARY_WARN"
+  autom8_key_value "Falhas" "$AUTOM8_SUMMARY_FAIL"
+  autom8_key_value "Logs" "$AUTOM8_LOG_DIR"
 
   if [[ "${#AUTOM8_SUMMARY_ITEMS[@]}" -gt 0 ]]; then
-    printf '%s\n' "${AUTOM8_SUMMARY_ITEMS[@]}"
+    echo
+    for item in "${AUTOM8_SUMMARY_ITEMS[@]}"; do
+      case "$item" in
+        OK:*) autom8_status_ok "${item#OK: }" ;;
+        AVISO:*) autom8_status_warn "${item#AVISO: }" ;;
+        FALHA:*) autom8_status_fail "${item#FALHA: }" ;;
+        *) printf '  %s\n' "$item" ;;
+      esac
+    done
   fi
 }
